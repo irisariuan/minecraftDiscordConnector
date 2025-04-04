fdbef555f4b15feacfd390c05fe6720ca0028aa5
irisariuan <kms12836@gmail.com>
2025-04-05 01:58:05 +0800

    Fix command file path resolution in loadCommands function

--- lib/commands.ts
+++ lib/commands.ts
@@ -1,8 +1,9 @@
 import type { ChatInputCommandInteraction, Client, SlashCommandBuilder } from "discord.js";
+import { join } from 'node:path'
 
 export interface CommandFile {
     command: SlashCommandBuilder
-    execute: (interaction: ChatInputCommandInteraction ,client: Client) => void | Promise<void>
+    execute: (interaction: ChatInputCommandInteraction, client: Client) => void | Promise<void>
 }
 
 export function loadCommands() {
@@ -10,7 +11,7 @@ export function loadCommands() {
     const commands: CommandFile[] = [];
 
     for (const path of glob.scanSync(process.cwd())) {
-        const commandFile = require(path).default;
+        const commandFile = require(join(process.cwd(), path)).default;
         if (!commandFile) continue
         commands.push(commandFile);
     }
